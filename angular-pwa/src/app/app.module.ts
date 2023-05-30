b18a26c26be1edd04bd742ba87eeae614277fdc9
Joao Vitor Franciscon Jacomo <joao.franciscon.jacomo@gmail.com>
2023-05-29 22:49:43 -0300

    atualizacao

--- angular-pwa/src/app/app.module.ts
+++ angular-pwa/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CardCarroComponent } from './card-carro/card-carro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    CardCarroComponent
   ],
   imports: [
     BrowserModule
