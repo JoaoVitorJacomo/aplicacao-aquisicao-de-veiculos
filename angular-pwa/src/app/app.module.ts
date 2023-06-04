4bbab1224305f21a9b291f6884240f8933488a0c
Joao Vitor Franciscon Jacomo <joao.franciscon.jacomo@gmail.com>
2023-06-04 14:38:04 -0300

    Revert "atualização topo e rodapé"
    This reverts commit 73ce10518d22817e198a6abc0dd64b8982ada473.

--- angular-pwa/src/app/app.module.ts
+++ angular-pwa/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { CardCarroComponent } from './card-carro/card-carro.component';
-import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
-    CardCarroComponent,
-    TopoComponent
+    CardCarroComponent
   ],
   imports: [
     BrowserModule
--- angular-pwa/src/app/rodape/rodape.component.ts
+++ angular-pwa/src/app/rodape/rodape.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./rodape.component.css']
 })
 export class RodapeComponent {
-  
+
 }
