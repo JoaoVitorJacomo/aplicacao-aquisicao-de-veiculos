776618e734a16274d81a3ab294b1f6b99abe4fa0
Joao Vitor Franciscon Jacomo <joao.franciscon.jacomo@gmail.com>
2023-07-16 09:41:59 -0300

    Aplicação PWA

--- angular-pwa/src/app/app-routing.module.ts
+++ angular-pwa/src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
 import {LandPageComponent} from './land-page/land-page.component';
 import {HomeComponent} from './home/home.component';
-import {CadastroPageComponent} from './cadastro-page/cadastro-page.component'
 
 const routes: Routes = [
   {path : 'app-land-page', component: LandPageComponent},
-  // {path : '', component: HomeComponent},
-  {path : '', component: CadastroPageComponent}
+  { path: '', redirectTo: 'app-land-page', pathMatch: 'full' },
+  { path: '**', component: LandPageComponent }
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
