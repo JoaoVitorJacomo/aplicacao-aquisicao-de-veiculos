fd41c4ae2e80b85c1d790438e1c7244b9e8d129f
Joao Vitor Franciscon Jacomo <joao.franciscon.jacomo@gmail.com>
2023-07-18 07:04:59 -0300

    pwa

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -9,16 +9,26 @@ export class TopoComponent {
    menuItemUm: string = "Cadastros";
    menuItemDois: string = "Sobre nós";
    menuItemTres: string = "Cadastrar Veículo";
-   menuItemQuatro: string = "Sair";
+   menuItemQuatro: string = "Deslogar";
    deslogar(){
      alert("Deslogando.");
    }
+   desaparecerMenu(){
+    let elemento = document.querySelector('.bg-menu');
+    let menuLateralTopo = document.querySelector('.topo .container');
+    elemento?.setAttribute('style','display:none;');
+    menuLateralTopo?.setAttribute('style','left:-250px;');
+   }
    abrirMenu(){
      let elemento = document.querySelector('html');
+     let bgMenu = document.querySelector('.bg-menu');
+     let menuLateralTopo = document.querySelector('.topo .container');
      if(elemento?.classList.contains("active")){
        elemento?.classList.remove("active");
      }else{
-       elemento?.classList.add("active");
+        bgMenu?.setAttribute('style','display:block;');
+        menuLateralTopo?.setAttribute('style','left:0px;');
+        elemento?.classList.add("active");
      }
    }
 }
